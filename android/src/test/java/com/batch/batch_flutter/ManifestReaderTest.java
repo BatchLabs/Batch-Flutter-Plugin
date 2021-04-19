f4e6654005b2d5d31a4980aff8b65813dfffdfdf
Arnaud Barisain-Monrose <arnaud@batch.com>
2021-04-19 15:23:56 +0200

    android: add manifest test

--- android/src/test/java/com/batch/batch_flutter/ManifestReaderTest.java
+++ android/src/test/java/com/batch/batch_flutter/ManifestReaderTest.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Assert;
@@ -17,9 +18,23 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ManifestReaderTest {
+    private static final String EXPECTED_BOOL_KEY = "tests_bool";
+    private static final String EXPECTED_STRING_KEY = "tests_string";
+
     @Test
-    public void test() {
-        boolean trueBool = true;
-        Assert.assertTrue(trueBool);
+    public void testBool() {
+        ManifestReader manifestReader = new ManifestReader(ApplicationProvider.getApplicationContext());
+        Assert.assertTrue(manifestReader.readBoolean(EXPECTED_BOOL_KEY, false));
+        Assert.assertFalse(manifestReader.readBoolean("missing", false));
+        Assert.assertTrue(manifestReader.readBoolean("missing", true));
+    }
+
+    @Test
+    public void testString() {
+        ManifestReader manifestReader = new ManifestReader(ApplicationProvider.getApplicationContext());
+        Assert.assertEquals("stringvalue", manifestReader.readString(EXPECTED_STRING_KEY, null));
+        Assert.assertNull(manifestReader.readString(EXPECTED_BOOL_KEY, null));
+        Assert.assertNull(manifestReader.readString("missing", null));
+        Assert.assertEquals("fallback", manifestReader.readString("missing", "fallback"));
     }
 }
