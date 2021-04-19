7c5e8e657daf6e9e4d8d9333e934aef14286af48
Arnaud Barisain-Monrose <arnaud@batch.com>
2021-04-19 12:57:05 +0200

    android: add unit test support to the project

---
+++ android/src/test/java/com/batch/batch_flutter/ManifestReaderTest.java
@@ -0,0 +1,25 @@
+package com.batch.batch_flutter;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Simple manifest reader
+ */
+@RunWith(AndroidJUnit4.class)
+public class ManifestReaderTest {
+    @Test
+    public void test() {
+        boolean trueBool = true;
+        Assert.assertTrue(trueBool);
+    }
+}
---
+++ android/src/test/java/com/batch/batch_flutter/TestActivity.java
@@ -0,0 +1,8 @@
+package com.batch.batch_flutter;
+
+import android.app.Activity;
+
+public class TestActivity extends Activity
+{
+
+}
