bb6a508f8ab5713591a2f62ecacfcea7aa442246
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-04-21 19:38:37 +0200

    tests: add installationID flutter test

---
+++ test/batch_user_test.dart
@@ -0,0 +1,26 @@
+import 'package:batch_flutter/batch_user.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const MethodChannel coreChannel = MethodChannel('batch_flutter');
+  const MethodChannel userChannel = MethodChannel('batch_flutter.user');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {});
+
+  tearDown(() {
+    userChannel.setMockMethodCallHandler(null);
+  });
+
+  test('getInstallationID', () async {
+    userChannel.setMockMethodCallHandler((MethodCall methodCall) async {
+      if (methodCall.method == 'user.getInstallationID') {
+        return "abcdef-ghij";
+      }
+    });
+
+    expect(await BatchUser.instance.installationID, 'abcdef-ghij');
+  });
+}
