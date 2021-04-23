3e58754490b086236cd3b208c7602355612b3c3b
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-04-23 18:25:13 +0200

    iOS: WIP bridge actions

---
+++ ios/Classes/Interop/Action.swift
@@ -0,0 +1,21 @@
+enum Action: String {
+    case optIn = "optIn"
+    case optOut = "optOut"
+    case optOutAndWipeData = "optOutAndWipeData"
+    
+    case push_GetLastKnownPushToken = "push.getLastKnownPushToken"
+    case push_iOSRequestPermission = "push.iOS.requestPermission"
+    case push_iOSRefreshToken = "push.iOS.refreshToken"
+    case push_clearBadge = "push.clearBadge"
+    case push_dismissNotifications = "push.dismissNotifications"
+    case push_iOSSetShowForegroundNotifications = "push.setIOSShowForegroundNotifications"
+    
+    case user_getIdentifier = "user.getIdentifier"
+    case user_getLanguage = "user.getLanguage"
+    case user_getRegion = "user.getRegion"
+    case user_getInstallationID = "user.getInstallationID"
+    
+    case debug_showDebugView = "debug.showDebugView"
+    
+    case echo = "echo"
+}
