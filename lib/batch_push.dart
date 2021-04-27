57d5dbfbc09cc388fbe10aa997b499093a40af03
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-04-27 15:30:01 +0200

    plugin: more dartdoc

--- lib/batch_push.dart
+++ lib/batch_push.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/services.dart';
 
+/// Provides push related functionality.
+/// Do not instanciate this: use the `instance` static property.
 class BatchPush {
   static const MethodChannel _channel =
       const MethodChannel('batch_flutter.push');
@@ -7,6 +9,16 @@ class BatchPush {
   /// Batch User module singleton.
   static BatchPush instance = new BatchPush();
 
+  /// Get the last known push token.
+  /// This might be null if the SDK never successfully registered to push notifcations.
+  ///
+  /// A number of issues can cause this: Invalid configuration, no permission
+  /// to display notifications,
+  /// running on an iOS simulator, Firebase unavailability, ...
+  /// See your logs for more information.
+  ///
+  /// Note: Calling this method too early in the application lifecycle might
+  /// return an out of date value.
   Future<String?> get lastKnownPushToken async {
     return await _channel.invokeMethod('push.getLastKnownPushToken');
   }
