61a3a920b37f786edff59038a5f354ce2dd4b50d
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-05-12 12:07:35 +0200

    plugin: add consent (optin) methods

--- lib/batch.dart
+++ lib/batch.dart
@@ -25,4 +25,49 @@ class Batch {
   void showDebugView() {
     _channel.invokeMethod('debug.showDebugView');
   }
+
+  /// Opt-in to Batch SDK.
+  /// Will automatically restart the SDK with the configured API Key.
+  void optIn() {
+    _channel.invokeMethod('optIn');
+  }
+
+  /// Opt-out from Batch SDK.
+  /// A opt-out request will also be sent to Batch servers if possible.
+  /// The SDK will save the opt-out and stop running even if the network request
+  /// fails.
+  ///
+  /// Some modules might behave unexpectedly
+  /// when the SDK is opted out from.
+  ///
+  /// Opting out will:
+  ///  - Prevent the SDK from being started until you call `optIn()`
+  ///  - Disable any network capability from the SDK
+  ///  - Disable all In-App campaigns
+  ///  - Make the Inbox module return an error immediatly
+  ///  - Make any call to `BatchUserDataEditor.save()` do nothing
+  ///  - Make any "track" methods from BatchUser ineffective
+  ///
+  /// Even if you opt-in afterwards, data generated (such as user data or tracked events) while opted out WILL be lost.
+  ///
+  /// If you also want to delete user data, please see `Batch.optOutAndWipeData()`.
+  ///
+  /// To implement a consent request where Batch should not be enabled until
+  /// the user explicitly opts-in, please see our documentation for opting out of
+  /// Batch by default, rather than using this method.
+  void optOut() {
+    _channel.invokeMethod('optOut');
+  }
+
+  /// Opt-out from Batch SDK and wipe data.
+  /// An installation data wipe command will be sent to Batch's servers if the user is connected to the internet.
+  ///
+  /// See `Batch.optOut()` for more info.
+  ///
+  /// Asking to opt out and wipe data while the SDK is already opted out from
+  /// won't do anything: please temporarily opt-in so that the data wipe request
+  /// can be sent to Batch's servers.
+  void optOutAndWipeData() {
+    _channel.invokeMethod('optOutAndWipeData');
+  }
 }
