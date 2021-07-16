b4f07357325cf177a487656e1a310c2c1606e437
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-07-16 14:59:57 +0200

    sample: boot to batch store

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:batch_flutter/batch_push.dart';
+import 'package:batch_flutter_example/batch_store/root_tab_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
 import 'batch_store/data/model/app_state_model.dart';
 import 'batch_store/data/model/cart.dart';
-import 'main_menu.dart';
+import 'plugin_test_menu.dart';
 
 void main() {
   runApp(MultiProvider(
@@ -40,7 +41,7 @@ class _BatchExampleAppState extends State<BatchExampleApp> {
         appBar: AppBar(
           title: const Text('Batch Flutter example app'),
         ),
-        body: MainMenu(),
+        body: RootTabPage(),
       ),
     );
   }
