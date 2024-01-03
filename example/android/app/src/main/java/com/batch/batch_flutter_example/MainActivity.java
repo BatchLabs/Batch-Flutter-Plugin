d1ec2719e53c4eab29109cb2f305ef0a6bc810c2
Arnaud Roland <arnaud.roland.ar@gmail.com>
2024-01-03 14:47:58 +0100

    sample: add missing batch onNewIntent

--- example/android/app/src/main/java/com/batch/batch_flutter_example/MainActivity.java
+++ example/android/app/src/main/java/com/batch/batch_flutter_example/MainActivity.java
@@ -1,6 +1,23 @@
 package com.batch.batch_flutter_example;
 
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+
+import com.batch.android.Batch;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+        Batch.onNewIntent(this, intent);
+    }
 }
