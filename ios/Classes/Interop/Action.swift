261888e03727ce21cd855e8950e8f6457ac64656
Arnaud Roland <arnaud.roland@batch.com>
2024-07-17 11:43:37 +0200

    ios: fix wrong action name for track location

--- ios/Classes/Interop/Action.swift
+++ ios/Classes/Interop/Action.swift
@@ -24,7 +24,7 @@ enum Action: String {
     case profile_identify = "profile.identify"
     case profile_edit = "profile.edit"
     case profile_trackEvent = "profile.track.event"
-    case profile_trackLocation = "user.track.location"
+    case profile_trackLocation = "profile.track.location"
     
     case inbox_createInstallationFetcher = "inbox.createInstallationFetcher"
     case inbox_createUserFetcher = "inbox.createUserFetcher"
