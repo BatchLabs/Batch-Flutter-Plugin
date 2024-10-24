5fd9b9399700c02564d261637966252547f4984b
Arnaud Roland <arnaud.roland@batch.com>
2024-10-24 10:46:23 +0200

    sample: update dependencies

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import batch_flutter
 import Batch
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
